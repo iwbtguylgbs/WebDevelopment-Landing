6e9578788d2677b6d9fe59f74e0bd15add894b3f
owo <russiancountryballs123@gmail.com>
2022-02-03 01:11:52 +0300

    Form Upd
    -Создан простой рабочий сайт с формой, принимающей данные и способной их выводить

--- django/Landing/email_newsletter/views.py
+++ django/Landing/email_newsletter/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import PeopleList
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>It's Britney, Bitch!</h1>")
+    name = ''
+    city = ''
+    phone = ''
+    mail = ''
+
+    if request.method == 'POST':
+        name = request.POST['name']
+        city = request.POST['city']
+        phone = request.POST['phone']
+        mail = request.POST['mail']
+        
+    context = {
+        'name': name,
+        'city': city,
+        'phone': phone,
+        'mail': mail,
+    }
+    return render(request, 'email_newsletter/index.html', context)
 
 def test(request):
     return HttpResponse("<h1>I'm a free bitch, baby!</h1>")
