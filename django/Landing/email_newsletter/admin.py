2f3fa5869e2089866b7eeb3364e929b9999c676f
owo <russiancountryballs123@gmail.com>
2022-02-02 23:48:12 +0300

    Models-Admin Upd
    -Создана основа для базы данных, хранящих информацию о пользователях, оставивших свою информацию
    
    -Создана админка на основе, предлагаемой Django, с возможностью управлять базами данных приложений и редактированием пользователей
    
    -Появился бланк HTML-странички

--- django/Landing/email_newsletter/admin.py
+++ django/Landing/email_newsletter/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import PeopleList
 
 # Register your models here.
+
+class PeopleListAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "mail", "number")
+    list_display_links = ("id", "name", "mail", "number")
+
+admin.site.register(PeopleList, PeopleListAdmin)
